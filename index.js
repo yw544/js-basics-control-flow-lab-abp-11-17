f4e367b1a142319aa4c57d3cf1563aa5c70ce92d
York Wang <yw544@nyu.edu>
2017-11-22 07:33:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-function scuberGreetingForFeet(x)
+function scuberGreetingForFeet(feet)
 {
-  if (x is <= 400)
+  if (feet <= 400)
   return "This one is on me!";
 }
