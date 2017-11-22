9963528008020818349f3fd3d11ffca8eeda5608
York Wang <yw544@nyu.edu>
2017-11-22 08:04:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,14 +21,15 @@ function ternaryCheckCity(city)
   return "No go."
 }
 
-function switchOnCarmFromTip(tip)
+
+
+function switchOnCharmFromTip(tip)
 {
 if (tip === "generous")
-{
-return "Thank you so much."
-}
- else if (tip === "not as generous")
+{return "Thank you so much."}
+else if (tip === "not as generous")
  {return "Thank you."}
  else
  {return "Bye."}
+
  }
