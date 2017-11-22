0e3ac90eaee63eadcb55806c826c8c1ba1819be3
York Wang <yw544@nyu.edu>
2017-11-22 08:04:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet)
+{
+  if (feet <= 400)
+  return "This one is on me!"
+
+else if (feet < 2500)
+return "I will gladly take your thirty bucks."
+
+else if  (feet >2500)
+return "No can do."
+
+};
+
+function ternaryCheckCity(city)
+{
+  if (city === "NYC")
+  return "Ok, sounds good."
+
+  else (city !== "NYC")
+  return "No go."
+}
+
+
+
+function switchOnCharmFromTip(tip)
+{
+if (tip === "generous")
+{return "Thank you so much."}
+else if (tip === "not as generous")
+ {return "Thank you."}
+ else
+ {return "Bye."}
+
+ }
