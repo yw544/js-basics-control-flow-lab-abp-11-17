1a9342847f254c69852fb9ab0ae0967fbbccd379
York Wang <yw544@nyu.edu>
2017-11-22 07:41:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(feet)
 {
   if (feet <= 400)
-  return "This one is on me!";
+  return "This one is on me!"
 
 else (feet > 2000)
 return "I will gladly take your thirty bucks."
